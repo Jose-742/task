868c46a220d5c30386d9ae029b4cceac3aa2f21b
jose.bento <jose.bento@ati.to.gov.br>
2024-01-23 18:34:13 -0300

    first commit

---
+++ src/main/java/models/Task.java
@@ -0,0 +1,33 @@
+package models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = Task.TABLE_NAME)
+public class Task {
+	public static final String TABLE_NAME = "task";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false, updatable = false)
+	private User user;
+
+	@Column(name = "description", length = 255, nullable = false)
+	@NotBlank
+	@Size(min = 1, max = 255)
+	private String description;
+
+}
---
+++ src/main/java/models/User.java
@@ -0,0 +1,45 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+	public static final String TABLE_NAME = "user";
+
+	@Id
+	@Column(name = "id", unique = true)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "username", length = 100, nullable = false, unique = true)
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String username;
+
+	@Column(name = "username", length = 100, nullable = false, unique = true)
+	@NotBlank
+	@Size(min = 8, max = 60)
+	private String password;
+
+	@OneToMany(mappedBy = "user")
+	private List<Task> tasks = new ArrayList<Task>();
+
+}
