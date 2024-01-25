1fd807e26bd42939436d7f79628967c257ecc439
jose.bento <jose.bento@ati.to.gov.br>
2024-01-25 17:53:58 -0300

    Create UserDetailsServiceImpl and JWTUtil

--- src/main/java/com/task/configs/SecurityConfig.java
+++ src/main/java/com/task/configs/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.task.configs;
 
+import com.task.security.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -22,6 +27,14 @@ import static org.springframework.http.HttpMethod.POST;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
     private static final String[] PUBLIC_MATCHERS = {
             "/"
     };
@@ -32,6 +45,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(this.userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder());
+        this.authenticationManager = authenticationManagerBuilder.build();
+
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
@@ -42,19 +63,13 @@ public class SecurityConfig {
                         )
                         .sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
 
-        /*AuthenticationManagerBuilder authenticationManagerBuilder = http
-                .getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(this.userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder());
-        this.authenticationManager = authenticationManagerBuilder.build();
-
-        http.authorizeRequests()
+        /*http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated().and()
                 .authenticationManager(authenticationManager);
 
-        http.addFilter(new JWTAuthenticationFilter(this.authenticationManager, this.jwtUtil));
+       /* http.addFilter(new JWTAuthenticationFilter(this.authenticationManager, this.jwtUtil));
         http.addFilter(new JWTAuthorizationFilter(this.authenticationManager, this.jwtUtil,
                 this.userDetailsService));*/
 
--- src/main/java/com/task/repositories/UserRepository.java
+++ src/main/java/com/task/repositories/UserRepository.java
@@ -3,7 +3,11 @@ package com.task.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.task.models.User;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+
+    @Transactional(readOnly = true)
+    User findByUsername(String username);
 }
