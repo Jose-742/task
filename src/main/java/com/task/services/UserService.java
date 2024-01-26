add19afc9429e7f31aa392568033db2e18bcd2f8
jose.bento <jose.bento@ati.to.gov.br>
2024-01-25 21:56:08 -0300

    Set UserService Authorization

--- src/main/java/com/task/services/UserService.java
+++ src/main/java/com/task/services/UserService.java
@@ -1,19 +1,22 @@
 package com.task.services;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.task.models.ProfileEnum;
+import com.task.security.UserSpringSecurity;
+import com.task.services.exceptions.AuthorizationException;
 import com.task.services.exceptions.DataBindingViolationException;
 import com.task.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.task.models.User;
-import com.task.repositories.TaskRepository;
 import com.task.repositories.UserRepository;
 
 @Service
@@ -26,6 +29,11 @@ public class UserService {
 	private UserRepository userRepository;
 
 	public User findById(Long id) {
+		UserSpringSecurity userSpringSecurity = authenticated();
+		if (!Objects.nonNull(userSpringSecurity)
+				|| !userSpringSecurity.hasRole(ProfileEnum.ADMIN) && !id.equals(userSpringSecurity.getId()))
+			throw new AuthorizationException("Acesso negado!");
+
 		Optional<User> user = this.userRepository.findById(id);
 		return user.orElseThrow(
 				() -> new ObjectNotFoundException("Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
@@ -53,6 +61,14 @@ public class UserService {
 			this.userRepository.deleteById(id);
 		} catch (Exception e) {
 			throw new DataBindingViolationException("Não é possivel excluir pois há entidades relacionadas!");
+		}
+	}
+
+	public static UserSpringSecurity authenticated(){
+		try {
+			return (UserSpringSecurity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		} catch (Exception e){
+			return null;
 		}
 	}
 
